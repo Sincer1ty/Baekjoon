436247df5045e9ee25db1c4cbee4340f56e1fea4
김성희 <87846251+Sincer1ty@users.noreply.github.com>
2024-07-16 16:47:40 +0900

    [Gold V] Title: 최소비용 구하기, Time: 216 ms, Memory: 37380 KB -BaekjoonHub

--- 백준/Gold/1916. 최소비용 구하기/최소비용 구하기.py
+++ 백준/Gold/1916. 최소비용 구하기/최소비용 구하기.py
@@ -1,38 +1,60 @@
 import sys
 import heapq
 
+sys.setrecursionlimit(10**8)
+
+class Node:
+    def __init__(self, value) -> None:
+        self.adj = {}
+        self.value = value
+        self.distance = 9999999999
+
+    def __lt__(self, other):
+        return  self.distance < other.distance
+         
+def Dijkstra(start:Node):
+    q = []
+    heapq.heappush(q, start)
+    
+    q.append(start)
+
+    
+    while len(q):
+        cur = heapq.heappop(q)
+        nodes = list(cur.adj.keys())
+        for n in nodes:
+            if n.distance > cur.distance+cur.adj[n]:
+                n.distance = cur.distance+cur.adj[n]
+                heapq.heappush(q, n)
+
+# 입력
 N = int(sys.stdin.readline())
 M = int(sys.stdin.readline())
 
-# 노드 별 경로 초기화
-vertex =[None]
-adj = {}
-q = []
-for n in range(N):
-    vertex.append(10**8)
-    adj[n+1] = []
+# 노드 배열 초기화
+nodes = [None]
+for i in range(N):
+    nodes.append(Node(i+1))
 
-for m in range(M):
-    u, v, w = list(map(int, sys.stdin.readline().split()))
-    adj[u].append((v, w))
+# 간선 배열 초기화
+for _ in range(M):
+    input = sys.stdin.readline().split()
+    a = nodes[int(input[0])]
+    b = nodes[int(input[1])]
+    dist = int(input[2])
+    
+    # 중복 간선 있을 경우 비용 높은건 무시
+    if b in a.adj and a.adj[b] < dist:
+        continue
+    a.adj[b] = dist
 
-start, dest = list(map(int, sys.stdin.readline().split()))
+# 시작, 끝점
+input = sys.stdin.readline().split()
+start = nodes[int(input[0])]
+end = nodes[int(input[1])]
 
-vertex[start] = 0
+# 로직 구현
+start.distance = 0
+Dijkstra(start)
 
-for n in range(N):
-    heapq.heappush(q, [vertex[n+1], n+1])
-
-while len(q):
-    u = heapq.heappop(q)
-
-    for a in adj[u[1]]:
-
-        if vertex[a[0]] > vertex[u[1]]+a[1]:
-            vertex[a[0]] = vertex[u[1]]+a[1]
-            for h in q:
-                if h[1] == a[0]:
-                    h[0] = vertex[a[0]]
-    heapq.heapify(q)
-
-print(vertex[dest])
+print(end.distance)
