50fc4d10176bf3f66b62660bf583a374dbd3c4dd
김성희 <87846251+Sincer1ty@users.noreply.github.com>
2025-10-15 23:56:29 +0900

    [level 0] Title: 머쓱이보다 키 큰 사람, Time: 0.01 ms, Memory: 4.23 MB -BaekjoonHub

---
+++ 프로그래머스/0/120585. 머쓱이보다 키 큰 사람/머쓱이보다 키 큰 사람.cpp
@@ -0,0 +1,14 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(vector<int> array, int height) {
+    int answer = 0;
+    
+    for (int a : array) {
+        if (a > height) ++answer;
+    }
+    
+    return answer;
+}
